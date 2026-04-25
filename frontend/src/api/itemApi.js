eaa5d2b693e650263c859d7b9d6041618136512b
IT24103981 <IT24103981@my.sliit.lk>
2026-04-26 01:47:31 +0530

    backend url updated with correct one

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "wmtlabtest1.railway.internal"
+  baseURL: import.meta.env.VITE_API_URL || "wmtlabtest1-production.up.railway.app"
 });
 
 export const getItems = () => API.get("/items");
